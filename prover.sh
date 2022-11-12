e0925497fa8de5044c1970d31a441bff9b8458e5
Bang Pateng <38981255+bangpateng@users.noreply.github.com>
2022-11-12 15:08:48 +0700

    prover

---
+++ prover.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+clear
+echo "==================================================================="
+echo -e "\e[92m"
+echo  "   ____          ____       _                    ";
+echo  "  | __ )  __ _  |  _ \ __ _| |_ ___ _ __   __ _  ";
+echo  "  |  _ \ / _' | | |_) / _' | __/ _ \ '_ \ / _' | ";
+echo  "  | |_) | (_| | |  __/ (_| | ||  __/ | | | (_| | ";
+echo  "  |____/ \__, | |_|   \__,_|\__\___|_| |_|\__, | ";
+echo  "         |___/                            |___/  ";
+echo -e "\e[0m"
+echo "===================================================================" 
+
+echo -e '\e[36mGarapan :\e[39m' Aleo Prover Testnet
+echo -e '\e[36mAuthor :\e[39m' Bang Pateng
+echo -e '\e[36mTelegram Group :\e[39m' @bangpateng_group
+echo -e '\e[36mTelegram Channel :\e[39m' @bangpateng_airdrop
+echo -e '\e[36mYoutube :\e[39m' www.youtube.com/c/BangPateng/
+echo -e '\e[36mWebsite :\e[39m' www.bangpatengnode.site
+
+sleep 2
+
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+# update
+cd $HOME
+sudo apt-get update && sudo apt install git
+
+echo -e "\e[1m\e[32m2. Open Port... \e[0m" && sleep 1
+ufw allow 22 && ufw allow 4130/tcp && ufw allow 4180/tcp && ufw enable
+
+echo -e "\e[1m\e[32m3. Install Rupstup... \e[0m" && sleep 1
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo -e "\e[1m\e[32m4. Clone Repository... \e[0m" && sleep 1
+cd $HOME
+git clone https://github.com/AleoHQ/snarkOS.git --depth 1 && cd snarkOS
+
+echo -e "\e[1m\e[32m5. Build... \e[0m" && sleep 1
+./build_ubuntu.sh
+
+echo -e "\e[1m\e[32m6. Cargo Path Install... \e[0m" && sleep 1
+cargo install --path .
+
+echo -e "\e[1m\e[32m2. Run Aleo Prover... \e[0m" && sleep 1
+snarkos account new
+
+echo '=============== SETUP FINISH BANG SIMPAN DATA DI ATAS PRIVATE KEY DAN LAINNYA ==================='&& sleep 1
