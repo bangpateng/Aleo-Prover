9dbecfe009b6be57927cc5cf2013c5e629ae0317
Bang Pateng <38981255+bangpateng@users.noreply.github.com>
2022-11-12 15:48:37 +0700

    Update prover.sh

--- prover.sh
+++ prover.sh
@@ -26,16 +26,17 @@ cd $HOME
 sudo apt-get update && sudo apt install git && sudo apt install screen
 
 echo -e "\e[1m\e[32m2. Open Port... \e[0m" && sleep 1
-ufw allow 22 && ufw allow 4130/tcp && ufw allow 4180/tcp && ufw enable
+ufw allow 22 && ufw allow 4133/tcp && ufw allow 3033/tcp && ufw enable
 
 echo -e "\e[1m\e[32m3. Install Rupstup... \e[0m" && sleep 1
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 echo -e "\e[1m\e[32m4. Clone Repository... \e[0m" && sleep 1
 cd $HOME
-git clone https://github.com/AleoHQ/snarkOS.git --depth 1 && cd snarkOS
+git clone https://github.com/AleoHQ/snarkOS.git --depth 1
 
 echo -e "\e[1m\e[32m5. Build... \e[0m" && sleep 1
+cd snarkOS
 ./build_ubuntu.sh
 
 echo -e "\e[1m\e[32m6. Cargo Path Install... \e[0m" && sleep 1
